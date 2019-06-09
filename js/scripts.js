186661c61ee8104aa19c5b21474c119bcc584efa
Janice-M <janiceink001@gmail.com>
2019-06-09 20:40:02 +0300

    click functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
   $(".whatwedo p").hide();
 
+  $('.whatwedo img').click(function() {
+    $('.whatwedo p').show();
+    $('.whatwedo img').hide();
+    });
+
+    $('.whatwedo p').click(function() {
+      $('.whatwedo img').show();
+      $('.whatwedo p').hide();
+    });
 
 });
